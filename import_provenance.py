94d08d89a348750103bf1c371192f956091cd383
FrancisMB2001 <franciscobarroscloud@gmail.com>
2025-08-22 16:17:32 +0000

    dockerized importer, also automatized it and added to docker compose to run it everyday at 3AM.

--- import_provenance.py
+++ import_provenance.py
@@ -1,14 +1,26 @@
+import os
 from neo4j import GraphDatabase
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "floresta_limpa_2025")
+NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
+NEO4J_CONNECTION = os.getenv("NEO4J_CONNECTION", "bolt://localhost:7687")
+
+NODES_LOCATION = os.getenv("NODES_LOCATION", "provenance_nodes.jsonl")
+RELATIONS_LOCATION = os.getenv("RELATIONS_LOCATION", "provenance_relations.jsonl")
 
 # Connect to Neo4j
-driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "floresta_limpa_2025"))
+driver = GraphDatabase.driver(NEO4J_CONNECTION, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
 # Load JSONL data
-with open("provenance_nodes.jsonl") as f:
+with open(NODES_LOCATION) as f:
     nodes = [json.loads(line) for line in f]
 
-with open("provenance_relations.jsonl") as f:
+with open(RELATIONS_LOCATION) as f:
     relations = [json.loads(line) for line in f]
 
 def insert_nodes(tx, node):
